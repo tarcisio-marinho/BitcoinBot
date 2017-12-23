b26fe9af3e3ad6b84764ef448509cc3e08dbc569
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-12-23 01:34:13 -0300

    info and removing unnecessary code

--- consulta.py
+++ consulta.py
@@ -1,5 +1,16 @@
 import requests, json, os, datetime, sqlite3, logging, time
 
+""" INFO
+last: preço em Reais baseado no último trade de cada exchange e ponderado pelo volume do período
+high: maior valor em Reais de uma operação no período
+low: menor valor em Reais de uma operação no período
+vol: volume em BTC de operações no período
+vwap: preço médio em Reais no período (volume weighted average price)
+money: volume em Reais de operações no período
+trades: número de operações no período
+
+"""
+
 def convert_timestamp(timestamp):
     return datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
 
@@ -77,37 +88,6 @@ def request_API():
         log(None, "CRITICAL", "Muitas requisições feitas, se continuar, não poderá fazer mais checagens.")
 
 
-def open_file():
-    f = open("data.json")
-    data = f.read()
-    data = json.loads(data)
-    
-    request_date = convert_timestamp(data["timestamp"]["exchanges"]["FOX"])
-    print("time", request_date)
-
-    print("\n\n\n24horas",)
-    foxbit_exchanges_24h = data["ticker_24h"]["exchanges"]["FOX"]
-    for i in foxbit_exchanges_24h:
-        print(i ,":", foxbit_exchanges_24h[i])
-    print("\n\n\n12 horas",)
-    foxbit_exchanges_12h = data["ticker_12h"]["exchanges"]["FOX"]
-    for i in foxbit_exchanges_12h:
-        print(i ,":", foxbit_exchanges_12h[i])
-
-    print("\n\n\n1 hora",)
-    foxbit_exchanges_1h = data["ticker_1h"]["exchanges"]["FOX"]
-    for i in foxbit_exchanges_1h:
-        print(i ,":", foxbit_exchanges_1h[i])
-    """
-    last: preço em Reais baseado no último trade de cada exchange e ponderado pelo volume do período
-    high: maior valor em Reais de uma operação no período
-    low: menor valor em Reais de uma operação no período
-    vol: volume em BTC de operações no período
-    vwap: preço médio em Reais no período (volume weighted average price)
-    money: volume em Reais de operações no período
-    trades: número de operações no período
-
-    """
     
 if __name__ == "__main__":
 
